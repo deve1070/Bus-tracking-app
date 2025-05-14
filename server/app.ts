2e33d3380cccad6f2df99a8b904d995090a141e8
deve1070 <dkassa253@gmail.com>
2025-05-14 15:57:48 +0300

    division of responsibility and set up

---
+++ server/app.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI as string, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err: Error) => console.error(err));
+
+app.get('/', (req: Request, res: Response) => res.send('API running'));
+
+export default app;
